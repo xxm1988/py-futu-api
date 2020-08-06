57784d2dcd47f443f7ab15498592a7e0398bbc0e
encorexiao <encorexiao@futunn.com>
2020-08-06 16:23:48 +0800

    解决冲突

--- futu/examples/simple_filter_demo.py
+++ futu/examples/simple_filter_demo.py
@@ -1,60 +1,3 @@
-<<<<<<< HEAD:futu/examples/simple.py
-# -*- coding: utf-8 -*-
-from futu import *
-
-import pandas as pd
-pd.set_option('display.width',2000)
-pd.set_option('display.max_colwidth',100)
-pd.set_option('display.max_columns',100)
-
-quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
-
-"""-------------------------------------------------"""
-
-from futu.quote.quote_get_warrant import Request
-from futu.quote.quote_stockfilter_info import SimpleFilter
-
-field = SimpleFilter()
-field.filter_min = 100
-field.filter_max = 1000
-field.stock_field = StockField.CUR_PRICE
-field.is_no_filter = False
-field.sort = SortDir.ASCEND
-
-# field2 = SimpleFilter()
-# field2.filter_min = 100
-# field2.filter_max = 1000
-# field2.stock_field = StockField.VOLUME_RATIO
-# field2.sort = SortDir.ASCEND
-# field2.is_no_filter = False
-#
-# field3 = SimpleFilter()
-# field3.stock_field = StockField.CUR_PRICE_TO_HIGHEST52_WEEKS_RATIO
-# field3.is_no_filter = True
-
-ret, ls = quote_ctx.get_stock_filter(Market.HK, [field])
-last_page, all_count, ret_list = ls
-print(ret_list)
-
-"""-------------------------------------------------"""
-# req = Request()
-# req.sort_field = SortField.CODE
-# req.ascend = True
-# req.type_list = [WrtType.BEAR, WrtType.CALL]
-# req .issuer_list = [Issuer.CS, Issuer.CT, Issuer.EA]
-# print(quote_ctx.get_warrant("HK.00700", req))
-
-req = Request()
-req.sort_field = SortField.CODE
-req.ascend = True
-req.type_list = [WrtType.BEAR, WrtType.BULL]
-req.issuer_list = [Issuer.CS, Issuer.CT, Issuer.EA]
-
-print(quote_ctx.get_warrant("HK.00700", req))
-quote_ctx.close()
-
-
-=======
 # -*- coding: utf-8 -*-
 
 """演示如何使用股票筛选功能"""
@@ -89,4 +32,3 @@ print(ret_list)
 quote_ctx.close()
 
 
->>>>>>> c6d8737232b7342604a2c3c8808fc22682a8b3f7:futu/examples/simple_filter_demo.py
