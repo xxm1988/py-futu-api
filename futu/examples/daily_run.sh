5803419412a74f1e06d1d39d091c5b66b9d8d641
xxm1988 <529792174@qq.com>
2020-08-10 10:09:02 +0800

    update daily run shell script

--- futu/examples/daily_run.sh
+++ futu/examples/daily_run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "[`date '+%Y-%m-%d %H:%M:%S'`]begin to run save_data" >> run.log
+echo "[`date '+%Y-%m-%d %H:%M:%S'`]begin to run save_data" >> /data/py-futu-api/futu/examples/run.log
 cd /data/py-futu-api/futu/examples
 
 # Source global definitions
@@ -10,6 +10,6 @@ fi
 
 source /root/.bashrc
 
-/root/anaconda3/envs/py37/bin/python ./save_data.py
+/root/anaconda3/envs/py37/bin/python ./save_data.py  >> /data/py-futu-api/futu/examples/save_data.log
 
-echo "[`date '+%Y-%m-%d %H:%M:%S'`]finish save_data" >> run.log
+echo "[`date '+%Y-%m-%d %H:%M:%S'`]finish save_data" >> /data/py-futu-api/futu/examples/run.log
